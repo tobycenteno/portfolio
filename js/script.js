562401ebd77502f70184b728f82cb02237edf23f
tobycenteno <59742161+tobycenteno@users.noreply.github.com>
2021-02-17 10:57:46 -0700

    update - 20210217

--- js/script.js
+++ js/script.js
@@ -1,24 +1,48 @@
 let percent = document.querySelector('.percent');
 let body = document.querySelector('body');
+let loader = document.querySelector('.home-loader');
 let val = 0;
 
 
-setTimeout(() => {
-   var timer = setInterval(function(){
-    if(val >= 99){
-        clearInterval(timer);
+if(loader) {
+  if (!sessionStorage.getItem( 'doNotShow' ) ) {
+    sessionStorage.setItem( 'doNotShow', true );
+    setTimeout(() => {
+      var timer = setInterval(function(){
+        if(val >= 99){
+          clearInterval(timer);
+       }
+       val++;
+       percent.innerHTML = val;
+      },30);
+    }, 500);
+   
+    setTimeout(() => {
+       body.classList.add('loaded');
+    }, 4000);
+  
+    setTimeout(() => {
+      AOS.init();
+    }, 5000);
+  
+  } else {
+    loader.style.display = "none";
+    AOS.init();
+  }
+}
+
+$(document).ready(function() {
+  $('.image-link').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    image: {
+      verticalFit: false
     }
-    val++;
-    percent.innerHTML = val;
-  },30);
-}, 500);
-
-setTimeout(() => {
-    body.classList.add('loaded');
-}, 4000);
+  });
+});
+  
 
 
-setTimeout(() => {
-  AOS.init();
-}, 5000);
+
+
 
